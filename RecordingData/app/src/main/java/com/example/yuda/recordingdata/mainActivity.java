dc5478af38970b8915751e47cb4381da875563cf
YuDa at NCKU <v420746@gmail.com>
2017-11-19 00:49:57 +0800

    add auto-recording on-time

--- RecordingData/app/src/main/java/com/example/yuda/recordingdata/mainActivity.java
+++ RecordingData/app/src/main/java/com/example/yuda/recordingdata/mainActivity.java
@@ -14,12 +14,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import jxl.*;
 import jxl.read.biff.BiffException;
@@ -38,9 +42,10 @@ import jxl.write.WriteException;
  */
 
 public class mainActivity extends AppCompatActivity {
-    TextView txtMessage;
+    TextView txtMessage, txtShowingWifiData;
     EditText edtPositionX, edtPositionY, edtDelayTime, edtRecordingLimit;
     Button btnScanningWifiInfo,btnReadCreate, btnStartStop, btnCloseXls;
+    RadioButton rdb_set_choose, rdb_set_ontime;
     WifiManager mWifiManager;
     List<ScanResult> lstWifiInfoResult = null;
     String[] aryWifiInfo = new String[]{};
@@ -58,6 +63,9 @@ public class mainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setDynamicsLayout();
         txtMessage = (TextView)findViewById(R.id.txtMessage);
+        txtShowingWifiData = (TextView)findViewById(R.id.txtShowingWifiData);
+        rdb_set_choose = (RadioButton)findViewById(R.id.rdb_set_choose);
+        rdb_set_ontime = (RadioButton)findViewById(R.id.rdb_set_ontime);
         edtPositionX = (EditText)findViewById(R.id.edtPositionX);
         edtPositionY = (EditText)findViewById(R.id.edtPositionY);
         edtDelayTime = (EditText)findViewById(R.id.edtDelayTime);
@@ -70,8 +78,25 @@ public class mainActivity extends AppCompatActivity {
         lsvShowingInfo = (ListView)findViewById(R.id.lsvShowingfInfo);
 
 
-
-
+        rdb_set_choose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtMessage.setText("您已選擇 勾選模式，您必須點選想要的基地台才可以記錄RSSI");
+                rdb_set_ontime.setChecked(false);
+                btnScanningWifiInfo.setClickable(true);
+                btnScanningWifiInfo.setText("scan");
+            }
+        });
+        rdb_set_ontime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtMessage.setText("您已選擇 自動更新模式，在紀錄時會自動更新接收到的基地台");
+                rdb_set_choose.setChecked(false);
+                btnScanningWifiInfo.setClickable(false);
+                btnScanningWifiInfo.setText("(X)已開始自動掃描WIFI");
+                lsvShowingInfo.setAdapter(new ArrayAdapter(mainActivity.this,android.R.layout.simple_list_item_multiple_choice,new String[][]{}));
+            }
+        });
 
         // scanning wifi information
         btnScanningWifiInfo.setOnClickListener(new View.OnClickListener() {
@@ -108,96 +133,124 @@ public class mainActivity extends AppCompatActivity {
         btnReadCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (btnReadCreate.getText().toString().equals("excel")) {
-                    btnReadCreate.setText("select all");
-                    // read or create excel
-                    try {
-                        mJxlFile.getExcel();
-                    } catch (IOException | WriteException | BiffException e) {
-                        e.printStackTrace();
-                    }
+                if (rdb_set_choose.isChecked()) {
+                    if (btnReadCreate.getText().toString().equals("excel")) {
+                        btnReadCreate.setText("select all");
+                        // read or create excel
+                        try {
+                            mJxlFile.getExcel();
+                        } catch (IOException | WriteException | BiffException e) {
+                            e.printStackTrace();
+                        }
 
-                }
-                else if (btnReadCreate.getText().toString().equals("select all")) {
-                    btnReadCreate.setText("cancel all");
-                    for (int i=0; i<aryWifiInfo.length; i++) {
-                        lsvShowingInfo.setItemChecked(i,true);
-                    }
-                }
-                else {
-                    btnReadCreate.setText("select all");
-                    for (int i=0; i<aryWifiInfo.length; i++) {
-                        lsvShowingInfo.setItemChecked(i,false);
-                    }
-                }
-            }
-        });
-
-        // starting recoding wifi-data to Excle
-        btnStartStop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if (!edtDelayTime.getText().toString().equals("")) {
-                    if (Integer.parseInt(edtDelayTime.getText().toString())<=0) {
-                        Toast.makeText(getApplicationContext(),"Fault enter, the time value must be larger than 0. Times still is "
-                                +mJxlFile.delayTime,Toast.LENGTH_SHORT).show();
+                    } else if (btnReadCreate.getText().toString().equals("select all")) {
+                        btnReadCreate.setText("cancel all");
+                        for (int i = 0; i < aryWifiInfo.length; i++) {
+                            lsvShowingInfo.setItemChecked(i, true);
+                        }
                     } else {
-                        mJxlFile.delayTime = Integer.parseInt(edtDelayTime.getText().toString());
-                    }
-                } else { mJxlFile.delayTime = 1; }
-                if (!edtRecordingLimit.getText().toString().equals("")) {
-                    if (Integer.parseInt(edtRecordingLimit.getText().toString())<=0) {
-                        Toast.makeText(getApplicationContext(),"Fault enter, the recording total must be larger than 0. Limit still is "
-                                +mJxlFile.dataRecordsLimits,Toast.LENGTH_SHORT).show();
-                    } else {
-                        mJxlFile.dataRecordsLimits = Integer.parseInt(edtRecordingLimit.getText().toString());
-                    }
-                } else { mJxlFile.dataRecordsLimits = 30; }
-
-
-                if (btnReadCreate.getText().toString().equals("excel")) {
-                    Toast.makeText(getApplicationContext(),"Please press EXCEL before START.",Toast.LENGTH_SHORT).show();
-                }
-                else if (edtPositionX.getText().toString().equals("") ||
-                        edtPositionY.getText().toString().equals("")) {
-                    Toast.makeText(getApplicationContext(),"Please make sure position have been entered.",Toast.LENGTH_SHORT).show();
-                }
-                else if (lstWifiInfoResult == null) {
-                    Toast.makeText(getApplicationContext(),"Please make sure wifi have been scanned.",Toast.LENGTH_SHORT).show();
-                }
-                else if (btnStartStop.getText().toString().equals("start")) {
-
-                    // starting recording activities
-                    aryChoseItemsNameAndBssid = new String[2][lsvShowingInfo.getCheckedItemCount()];
-                    int count = 0;
-                    for (int i=0; i<lsvShowingInfo.getCheckedItemPositions().size(); i++) {
-                        if (lsvShowingInfo.getCheckedItemPositions().valueAt(i)) {
-                            aryChoseItemsNameAndBssid[0][i-count] = aryWifiName[lsvShowingInfo.getCheckedItemPositions().keyAt(i)];
-                            aryChoseItemsNameAndBssid[1][i-count] = aryWifiBssid[lsvShowingInfo.getCheckedItemPositions().keyAt(i)];
-
-                        } else {
-                            count++;
+                        btnReadCreate.setText("select all");
+                        for (int i = 0; i < aryWifiInfo.length; i++) {
+                            lsvShowingInfo.setItemChecked(i, false);
                         }
                     }
-                    if (aryChoseItemsNameAndBssid[0].length == 0) {
-                        Toast.makeText(getApplicationContext(),"You did not choose any item.",Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(getApplicationContext(),"You have chosen these items with: \n"+Arrays.deepToString(aryChoseItemsNameAndBssid[0])
-                                ,Toast.LENGTH_SHORT).show();
-                        btnStartStop.setText("stop");
-
+                }
+                if (rdb_set_ontime.isChecked()) {
+                    if (btnReadCreate.getText().toString().equals("excel")) {
+                        btnReadCreate.setText("read");
+                        // read or create excel
                         try {
-                            mJxlFile.startRecording(aryChoseItemsNameAndBssid);
-                        } catch (WriteException e) {
+                            mJxlFile.getExcel();
+                        } catch (IOException | WriteException | BiffException e) {
                             e.printStackTrace();
                         }
                     }
                 }
-                else {
-                    btnStartStop.setText("start");
-                    // stopping recording activities suddenly
-                    mJxlFile.stopRecording();
+
+            }
+        });
+
+        // starting recoding wifi-data to Excel
+        btnStartStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!edtDelayTime.getText().toString().equals("")) {
+                    if (Integer.parseInt(edtDelayTime.getText().toString()) <= 0) {
+                        Toast.makeText(getApplicationContext(), "Fault enter, the time value must be larger than 0. Times still is "
+                                + mJxlFile.delayTime, Toast.LENGTH_SHORT).show();
+                    } else {
+                        mJxlFile.delayTime = Integer.parseInt(edtDelayTime.getText().toString());
+                    }
+                } else {
+                    mJxlFile.delayTime = 1;
+                }
+                if (!edtRecordingLimit.getText().toString().equals("")) {
+                    if (Integer.parseInt(edtRecordingLimit.getText().toString()) <= 0) {
+                        Toast.makeText(getApplicationContext(), "Fault enter, the recording total must be larger than 0. Limit still is "
+                                + mJxlFile.dataRecordsLimits, Toast.LENGTH_SHORT).show();
+                    } else {
+                        mJxlFile.dataRecordsLimits = Integer.parseInt(edtRecordingLimit.getText().toString());
+                    }
+                } else {
+                    mJxlFile.dataRecordsLimits = 30;
+                }
+                if (btnReadCreate.getText().toString().equals("excel")) {
+                    Toast.makeText(getApplicationContext(), "Please press EXCEL before START.", Toast.LENGTH_SHORT).show();
+                } else if (edtPositionX.getText().toString().equals("") ||
+                        edtPositionY.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "Please make sure position have been entered.", Toast.LENGTH_SHORT).show();
+                }
+
+                // check the model which has been chosen
+                if (rdb_set_choose.isChecked()) {
+                    if (lstWifiInfoResult == null) {
+                        Toast.makeText(getApplicationContext(), "Please make sure wifi have been scanned.", Toast.LENGTH_SHORT).show();
+                    } else if (btnStartStop.getText().toString().equals("start")) {
+
+                        // starting recording activities
+                        aryChoseItemsNameAndBssid = new String[2][lsvShowingInfo.getCheckedItemCount()];
+                        int count = 0;
+                        for (int i = 0; i < lsvShowingInfo.getCheckedItemPositions().size(); i++) {
+                            if (lsvShowingInfo.getCheckedItemPositions().valueAt(i)) {
+                                aryChoseItemsNameAndBssid[0][i - count] = aryWifiName[lsvShowingInfo.getCheckedItemPositions().keyAt(i)];
+                                aryChoseItemsNameAndBssid[1][i - count] = aryWifiBssid[lsvShowingInfo.getCheckedItemPositions().keyAt(i)];
+
+                            } else {
+                                count++;
+                            }
+                        }
+                        if (aryChoseItemsNameAndBssid[0].length == 0) {
+                            Toast.makeText(getApplicationContext(), "You did not choose any item.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "You have chosen these items with: \n" + Arrays.deepToString(aryChoseItemsNameAndBssid[0])
+                                    , Toast.LENGTH_SHORT).show();
+                            btnStartStop.setText("stop");
+
+                            try {
+                                mJxlFile.startRecording(aryChoseItemsNameAndBssid);
+                            } catch (WriteException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    } else {
+                        btnStartStop.setText("start");
+                        // stopping recording activities suddenly
+                        mJxlFile.stopRecording();
+                    }
+                }
+                if (rdb_set_ontime.isChecked()) {
+                    if (btnStartStop.getText().toString().equals("start")) { //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                        try {
+                            mJxlFile.startAllRecording();
+                        } catch (WriteException e) {
+                            e.printStackTrace();
+                        }
+                        btnStartStop.setText("stop");
+                    } else {
+                        btnStartStop.setText("start");
+                        // stopping recording activities suddenly
+                        mJxlFile.stopRecording();
+                    }
                 }
             }
         });
@@ -206,20 +259,19 @@ public class mainActivity extends AppCompatActivity {
         btnCloseXls.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                if (btnReadCreate.getText().toString().equals("excel")) {
-                    Toast.makeText(getApplicationContext(),"Please press EXCEL before FINISH.",Toast.LENGTH_SHORT).show();
-                } else {
-                    try {
-                        mJxlFile.stopRecording();
-                        mJxlFile.closeWork();
-                    } catch (IOException | WriteException | BiffException e) {
-                        e.printStackTrace();
-                    }
-                    btnReadCreate.setText("excel");
-                    btnStartStop.setText("start");
-
+            if (btnReadCreate.getText().toString().equals("excel")) {
+                Toast.makeText(getApplicationContext(),"Please press EXCEL before FINISH.",Toast.LENGTH_SHORT).show();
+            } else {
+                try {
+                    mJxlFile.stopRecording();
+                    mJxlFile.closeWork();
+                } catch (IOException | WriteException | BiffException e) {
+                    e.printStackTrace();
                 }
+                btnReadCreate.setText("excel");
+                btnStartStop.setText("start");
+
+            }
             }
         });
 
@@ -237,8 +289,6 @@ public class mainActivity extends AppCompatActivity {
         int delayTime = 1;
         int dataRecordsLimits = 30;
         int count  = 1;
-
-
 
         void getExcel() throws IOException, WriteException, BiffException {
             if (!mFolder.exists()) {
@@ -261,6 +311,69 @@ public class mainActivity extends AppCompatActivity {
             this.copyToWritableSheet = this.copyToWritable.getSheet(0);
         }
 
+        void startAllRecording() throws WriteException {
+            final LinkedHashMap<String, Integer> map_allWifiName = new LinkedHashMap();
+
+            final ArrayList<ArrayList<String>> dataList = new ArrayList<>();
+
+            this.runnable = new Runnable() {
+                @Override
+                public void run() {
+                    // recording process
+                    mWifiManager.startScan();
+                    List<ScanResult> results = mWifiManager.getScanResults();
+                    handler.postDelayed(this,delayTime*1000);
+                    if (count <= dataRecordsLimits) {
+                        txtMessage.setText("您正在紀錄第 "+count+" 筆資料，尚餘 " + (dataRecordsLimits-count) +" 筆資料");
+                        for (int i=0; i<results.size(); i++) {
+                            if (!map_allWifiName.containsKey(results.get(i).BSSID)) {
+                                map_allWifiName.put(results.get(i).BSSID, map_allWifiName.size());
+                                dataList.add(new ArrayList<String>());
+                                dataList.get(map_allWifiName.get(results.get(i).BSSID)).add(results.get(i).SSID);
+                                dataList.get(map_allWifiName.get(results.get(i).BSSID)).add(results.get(i).BSSID);
+                                for (int j=0; j<count-1; j++) {
+                                        dataList.get(map_allWifiName.get(results.get(i).BSSID)).add("");
+                                    }
+                            }
+                            dataList.get(map_allWifiName.get(results.get(i).BSSID)).add(results.get(i).level+"");
+                        }
+                        for (int i=0; i<dataList.size(); i++) {
+                            if (dataList.get(i).size()-2 < count) {
+                                dataList.get(i).add("");
+                            }
+                        }
+                        count++;
+                    } else {
+                        try {
+                            writeInBook(dataList);
+                        } catch (WriteException e) {
+                            e.printStackTrace();
+                        }
+                        stopRecording();
+                        txtMessage.setText("紀錄已完成");
+                    }
+                }
+            };
+            this.handler.post(this.runnable);
+
+        }
+        void writeInBook(ArrayList<ArrayList<String>> dataList) throws WriteException {
+            final int getExcelRows = this.copyToWritableSheet.getRows();
+            String positionX = edtPositionX.getText().toString();
+            String positionY = edtPositionY.getText().toString();
+            for (int i=0; i<dataList.size(); i++) {
+                for (int j=0; j<dataList.get(i).size(); j++) {
+                    if (j<2) {
+                        copyToWritableSheet.addCell(new Label(j,i+getExcelRows,dataList.get(i).get(j)));
+                    }
+                    else {
+                        copyToWritableSheet.addCell(new Label(2, i+getExcelRows, positionX));
+                        copyToWritableSheet.addCell(new Label(3, i+getExcelRows, positionY));
+                        copyToWritableSheet.addCell(new Label(j+2,i+getExcelRows,dataList.get(i).get(j)));
+                    }
+                }
+            }
+        }
         void startRecording(final String[][] choseWifiNameAndBssid) throws WriteException {
             final int getExcelRows = this.copyToWritableSheet.getRows();
             String positionX = edtPositionX.getText().toString();
